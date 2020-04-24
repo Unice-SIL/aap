04a89c03eb8e7b895eb5a534db795ebccd97c9a9
Lartist <salim.developpeur@gmail.com>
2020-04-24 15:07:34 +0200

    liste des projets done

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Project;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ProjectController
+ * @package App\Controller
+ * @Route("/project", name="project.")
+ */
+class ProjectController extends AbstractController
+{
+    /**
+     * @Route("/index", name="index", methods={"GET"})
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function index(EntityManagerInterface $em)
+    {
+        return $this->render('project/index.html.twig', [
+            'projects' => $em->getRepository(Project::class)->findBy(
+                ['createdBy' => $this->getUser()],
+                ['createdAt' => 'DESC'],
+            ),
+        ]);
+    }
+}
