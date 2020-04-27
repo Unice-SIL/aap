9891e66d5f147c64262321ba2cc5d7d78a032d4a
Lartist <salim.developpeur@gmail.com>
2020-04-27 15:55:01 +0200

    modification d'un projet done

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,8 +5,11 @@ namespace App\Controller;
 
 
 use App\Entity\Project;
+use App\Manager\Project\ProjectManagerInterface;
+use App\Widget\WidgetManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,6 +31,46 @@ class ProjectController extends AbstractController
             'projects' => $em->getRepository(Project::class)->findBy(
                 ['createdBy' => $this->getUser()],
                 ['createdAt' => 'DESC']
+            ),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="edit", methods={"GET", "POST"})
+     * @param Project $project
+     * @param WidgetManager $widgetManager
+     * @param Request $request
+     * @param ProjectManagerInterface $projectManager
+     * @return Response
+     * @throws \Exception
+     */
+    public function edit(
+        Project $project,
+        WidgetManager $widgetManager,
+        Request $request,
+        ProjectManagerInterface $projectManager
+    )
+    {
+        $projectManager->refreshProjectContents($project);
+
+        $dynamicForm = $widgetManager->getDynamicForm($project);
+
+        $dynamicForm->handleRequest($request);
+
+        if ($dynamicForm->isSubmitted() and $dynamicForm->isValid()) {
+
+            $widgetManager->hydrateProjectContentsByForm($project->getProjectContents(), $dynamicForm);
+
+            $projectManager->update($project);
+
+            return $this->redirectToRoute('app.project.edit', ['id' => $project->getId()]);
+        }
+
+        return $this->render('project/edit.html.twig', [
+            'project' => $project,
+            'dynamic_form_html' => $widgetManager->renderDynamicFormHtml(
+                $dynamicForm,
+                'partial/widget/_dynamic_form.html.twig'
             ),
         ]);
     }
--- src/Manager/Project/ProjectManagerInterface.php
+++ src/Manager/Project/ProjectManagerInterface.php
@@ -10,6 +10,8 @@ interface ProjectManagerInterface
 {
     public function create(CallOfProject $callOfProject): Project;
 
+    public function refreshProjectContents(Project $project): void;
+
     public function save(Project $project);
 
     public function update(Project $project);
