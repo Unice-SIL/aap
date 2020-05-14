733dbcd8132f2d0e580047f901e7102c613a386c
Lartist <salim.developpeur@gmail.com>
2020-05-14 14:23:38 +0200

    amélioratin du setting password lors de la soumission du formulaire

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -20,19 +20,33 @@ class UserFixtures extends Fixture
     const USER_USER1 = 'user1';
 
     /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    /**
+     * CallOfProjectSubscriber constructor.
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    /**
      * @param ObjectManager $manager
      */
     public function load(ObjectManager $manager)
     {
         $user = new User();
         $user->setUsername(self::USER_ADMIN)
-            ->setPlainPassword(self::USER_ADMIN);
+            ->setPassword($this->encoder->encodePassword($user, self::USER_ADMIN));
         $manager->persist($user);
         $this->addReference(self::class . self::USER_ADMIN, $user);
 
         $user = new User();
         $user->setUsername(self::USER_USER1)
-            ->setPlainPassword(self::USER_USER1);
+            ->setPassword($this->encoder->encodePassword($user, self::USER_USER1));
         $manager->persist($user);
         $this->addReference(self::class . self::USER_USER1, $user);
 
