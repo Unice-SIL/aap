4f71ef9ed70d3ef5b6f86aca621edb8ac9092ee0
IBEGHOUCHENE <Nadir.IBEGHOUCHENE@univ-cotedazur.fr>
2023-02-07 16:58:23 +0100

    fix : migration comment field

--- src/Migrations/Version20230207121457.php
+++ src/Migrations/Version20230207121457.php
@@ -22,9 +22,7 @@ final class Version20230207121457 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE notification CHANGE route_params route_params LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
         $this->addSql('ALTER TABLE project ADD comment LONGTEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
     }
 
     public function down(Schema $schema) : void
@@ -32,8 +30,6 @@ final class Version20230207121457 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE notification CHANGE route_params route_params LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
         $this->addSql('ALTER TABLE project DROP comment');
-        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
     }
 }
